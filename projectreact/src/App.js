e979c44dbc0cb295b6f5ab4aded1fcb6ae63080d
neaRRRRR <kaancakici510@gmail.com>
2020-02-25 13:38:31 +0300

    25.02.2020-13:38

--- projectreact/src/App.js
+++ projectreact/src/App.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import Tweet from "./tweet";
 
 function App() {
+  const [users, setUsers] = useState([
+    { name: "kaan", message: "hello" },
+    { name: "kaan1", message: "hello1" },
+    { name: "kaan2", message: "hello2" }
+  ]);
+
   return (
-    <div>
-      <h1>Hello React</h1>
+    <div className="app">
+      {users.map(user => (
+        <Tweet name={user.name} message={user.message} />
+      ))}
     </div>
   );
 }
